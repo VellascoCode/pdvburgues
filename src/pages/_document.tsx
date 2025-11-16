d2177df83c96d255f4a7f14ffef693cfbfb52c04
vellascocode <vellascocode@gmail.com>
2025-11-16 18:10:49 -0300

    feat: remover script de limpeza de Service Worker do documento

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,36 +7,6 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
         <link rel="icon" href="/assets/icons/pwa-icon.svg" type="image/svg+xml" />
-        {/* Remove qualquer Service Worker antigo antes de carregar os bundles */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(){
-                try {
-                  var flagKey = 'sw:cleaned';
-                  var shouldReload = false;
-                  if ('serviceWorker' in navigator) {
-                    navigator.serviceWorker.getRegistrations().then(function(regs){
-                      if (regs && regs.length) {
-                        shouldReload = true;
-                        regs.forEach(function(reg){ reg.unregister().catch(function(){}); });
-                      }
-                      if (shouldReload && !sessionStorage.getItem(flagKey)) {
-                        sessionStorage.setItem(flagKey, '1');
-                        window.location.reload();
-                      }
-                    }).catch(function(){});
-                  }
-                  if ('caches' in window) {
-                    caches.keys().then(function(keys){
-                      return Promise.all(keys.map(function(k){ return caches.delete(k); }));
-                    }).catch(function(){});
-                  }
-                } catch(e) {}
-              })();
-            `,
-          }}
-        />
         {/* Pre-apply saved theme to avoid flicker and ensure BG shows */}
         <script
           dangerouslySetInnerHTML={{
